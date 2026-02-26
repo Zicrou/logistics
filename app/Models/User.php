a8aa37586088ae730e0ecad39a9216ebcb70eaa9
Zicrou <aazseck@gmail.com>
2026-02-26 04:41:17 +0000

    Ajout de la relation User document dans User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -50,4 +50,8 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function documents(){
+        return $this->hasMany(Document::class);
+    }
 }
